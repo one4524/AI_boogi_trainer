9ea5d9fca8241d5750f216c311bd7c8eba5fb481
hojun-jo <86751964+hojun-jo@users.noreply.github.com>
2022-05-06 15:03:14 +0900

    Block night theme
    다크모드 막기
    음식 상세 정보 페이지 간편입력 탭에서 대, 중, 소 버튼 하나만 선택된 것처럼 보이게 테두리 효과

--- app/src/main/java/com/example/boogi_trainer/ui/food/FoodFragmentTab1.kt
+++ app/src/main/java/com/example/boogi_trainer/ui/food/FoodFragmentTab1.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.boogi_trainer.R
 import com.example.boogi_trainer.databinding.FragmentFoodTab1Binding
 
 class FoodFragmentTab1 : Fragment() {
@@ -17,5 +18,30 @@ class FoodFragmentTab1 : Fragment() {
     ): View? {
         binding = FragmentFoodTab1Binding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btSmall = binding.buttonSmall
+        val btMedium = binding.buttonMedium
+        val btLarge = binding.buttonlarge
+
+        // 버튼 클릭 효과(테두리) 하나에만 적용되게 만들기
+        btSmall.setOnClickListener {
+            btSmall.setBackgroundResource(R.drawable.bt_highlight)
+            btMedium.setBackgroundResource(R.color.white)
+            btLarge.setBackgroundResource(R.color.white)
+        }
+        btMedium.setOnClickListener {
+            btMedium.setBackgroundResource(R.drawable.bt_highlight)
+            btSmall.setBackgroundResource(R.color.white)
+            btLarge.setBackgroundResource(R.color.white)
+        }
+        btLarge.setOnClickListener {
+            btLarge.setBackgroundResource(R.drawable.bt_highlight)
+            btMedium.setBackgroundResource(R.color.white)
+            btSmall.setBackgroundResource(R.color.white)
+        }
     }
 }
