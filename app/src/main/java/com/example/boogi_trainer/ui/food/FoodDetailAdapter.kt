21ad25518a546530217232770699cd3f95915a5c
hojun-jo <86751964+hojun-jo@users.noreply.github.com>
2022-05-15 16:27:40 +0900

    food search in FoodDetailActivity
    FoodDetailActivity에서 음식 이름이 잘못되었을 때 검색해서 음식 이름 수정할 수 있게함

--- app/src/main/java/com/example/boogi_trainer/ui/food/FoodDetailAdapter.kt
+++ app/src/main/java/com/example/boogi_trainer/ui/food/FoodDetailAdapter.kt
@@ -2,8 +2,10 @@ package com.example.boogi_trainer.ui.food
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.boogi_trainer.databinding.RecyclerviewFoodListItemBinding
+import com.example.boogi_trainer.repository.APIManager
 
 class FoodDetailAdapter: RecyclerView.Adapter<DetailHolder>() {
     var foodData = mutableListOf<FoodDetailData>()
@@ -16,6 +18,7 @@ class FoodDetailAdapter: RecyclerView.Adapter<DetailHolder>() {
     override fun onBindViewHolder(holder: DetailHolder, position: Int) {
         val foodDetailData = foodData[position]
         holder.setData(foodDetailData, position)
+
     }
 
     override fun getItemCount(): Int {
@@ -24,8 +27,34 @@ class FoodDetailAdapter: RecyclerView.Adapter<DetailHolder>() {
 }
 
 class DetailHolder(val binding: RecyclerviewFoodListItemBinding): RecyclerView.ViewHolder(binding.root) {
+
+    init {
+        // 음식 검색 버튼을 누르면 높이 0으로 숨어있던 레이아웃의 높이를 변경해서 음식을 검색할 수 있는 editText와 버튼이 나온다
+        binding.buttonFoodSearch.setOnClickListener {
+            changeHeightLayoutFoodSearch(150)
+        }
+        // 확인 버튼을 누르면 editText에 임력된 음식 이름이 서버에 있는지 확인하고
+        // 있으면 listItemFoodName을 변경하고 높이를 0으로 만들어 레이아웃 숨김
+        binding.buttonCheck.setOnClickListener {
+            val name = binding.editTextFoodName.text.toString()
+            if (APIManager.getFood(name).name != "") {
+                binding.listItemFoodName.text = name
+                changeHeightLayoutFoodSearch(0)
+            }
+            else {
+                Toast.makeText(binding.root.context, "음식이 존재하지 않습니다", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     fun setData(foodDetailData: FoodDetailData, position: Int) {
         binding.listItemFoodName.text = foodDetailData.name
         binding.editTextGram.setText(foodDetailData.gram)
+    }
+
+    private fun changeHeightLayoutFoodSearch(height: Int) {
+        val layoutParams = binding.layoutFoodSearch.layoutParams
+        layoutParams.height = height
+        binding.layoutFoodSearch.layoutParams = layoutParams
     }
 }
